215f084cd17e76b3868fc99729842a23904d5d75
ALI <assfourali98@gmail.com>
2023-08-01 08:15:02 -0700

    change the dashboard to use UserController index function

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\User\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,8 @@ class HomeController extends Controller
     public function dashboard()
     {
         $user = auth()->user();
+        $UserController = app(UserController::class);
+        $users = $UserController->index();
         $users = User::where('role_id', '<>', '1')->get();
         return response()->json([
             'auth_user' => $user,
