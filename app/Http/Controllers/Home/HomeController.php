203d330aa17c532158f496075bd14a174bdb223a
ALI <assfourali98@gmail.com>
2023-08-08 09:19:31 -0700

    add swagger to Dashnoard

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -9,6 +9,141 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/home",
+     *     tags={"Dashboard"},
+     *     summary="User dashboard",
+     *     description="Retrieves the dashboard information for the authenticated user.",
+     *     security={
+     *         {"bearerAuth": {}}
+     *     },
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success response",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="auth_user",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="integer",
+     *                     format="int64",
+     *                     example=1
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string",
+     *                     example="John Doe"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="username",
+     *                     type="string",
+     *                     example="johndoe"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="certificate",
+     *                     type="array",
+     *                     @OA\Items(type="string", example="XYZ Certificate")
+     *                 ),
+     *                 @OA\Property(
+     *                     property="courses",
+     *                     type="array",
+     *                     @OA\Items(type="string", example="Course A")
+     *                 ),
+     *                 @OA\Property(
+     *                     property="directorate_name",
+     *                     type="string",
+     *                     example="Marketing"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="directorate_id",
+     *                     type="integer",
+     *                     format="int64",
+     *                     example=1
+     *                 ),
+     *                 @OA\Property(
+     *                     property="city_name",
+     *                     type="string",
+     *                     example="New York"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="city_id",
+     *                     type="integer",
+     *                     format="int64",
+     *                     example=1
+     *                 ),
+     *                 @OA\Property(
+     *                     property="phone",
+     *                     type="string",
+     *                     example="1234567890"
+     *                 )
+     *             ),
+     *             @OA\Property(
+     *                 property="users",
+     *                 type="array",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="id",
+     *                         type="integer",
+     *                         format="int64",
+     *                         example=2
+     *                     ),
+     *                     @OA\Property(
+     *                         property="name",
+     *                         type="string",
+     *                         example="Jane Smith"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="username",
+     *                         type="string",
+     *                         example="janesmith"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="certificate",
+     *                         type="array",
+     *                         @OA\Items(type="string", example="ABC Certificate")
+     *                     ),
+     *                     @OA\Property(
+     *                         property="courses",
+     *                         type="array",
+     *                         @OA\Items(type="string", example="Course B")
+     *                     ),
+     *                     @OA\Property(
+     *                         property="directorate_name",
+     *                         type="string",
+     *                         example="Sales"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="directorate_id",
+     *                         type="integer",
+     *                         format="int64",
+     *                         example=2
+     *                     ),
+     *                     @OA\Property(
+     *                         property="city_name",
+     *                         type="string",
+     *                         example="London"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="city_id",
+     *                         type="integer",
+     *                         format="int64",
+     *                         example=2
+     *                     ),
+     *                     @OA\Property(
+     *                         property="phone",
+     *                         type="string",
+     *                         example="9876543210"
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     ),
+     * )
+     */
     public function dashboard()
     {
         $user = auth()->user();
@@ -16,7 +151,7 @@ class HomeController extends Controller
         $users = $UserController->index();
         return response()->json([
             'auth_user' => $user,
-            'users'=>$users->original['users']
+            'users' => $users->original['users']
         ]);
     }
 }
