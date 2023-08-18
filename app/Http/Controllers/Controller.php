c0f06e8b0ed5c09c5b6c34d0c92ef721bf2de66a
ALI <assfourali98@gmail.com>
2023-08-18 08:16:59 -0700

    dd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,6 @@ use Illuminate\Routing\Controller as BaseController;
  * )
  * @OA\Server(
  *    url="http://127.0.0.1:8000/api",
- *     url = "https://center.lavetro-agency.com/api"
  * ),
  *   @OA\Server(
  *      url="https://center.lavetro-agency.com/api",
